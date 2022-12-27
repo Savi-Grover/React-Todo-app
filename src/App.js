34231df04dcbc9572dfddbcdda0f02a0921b7a64
Savi Grover <savig447@gmail.com>
2022-12-26 21:26:48 -0500

    Deletion and Updating tasks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import { useState } from 'react'
-
 // custom components
-import CustomForm from './Components/AddTodoForm'
+import CustomForm from './Components/CustomForm'
 import TaskList from './Components/TaskList'
-
+import EditForm from './Components/EditForm'
 function App() {
-  const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
+    const [editedTask, setEditedTask] = useState(null);
+    const [isEditing, setisEditing] = useState(false);
+    const [isPreviousFocusEl, setPreviousFocusEl] = useState(false);
 
-  const addTask = (task) => {
-    setTasks(prevState => [...prevState, task])
-  }
+    const addTask = (task) => {
+      setTasks(prevState => [...prevState, task])
 
-  const deleteTask = (id) => {
-    setTasks(prevState => prevState.filter(t => t.id !== id));
-  }
+    }
 
+    const deleteTask = (id) => {
+      setTasks(prevState => prevState.filter(t => t.id !== id));
+    }
+
+  /*--here toggleTask is taking the id, iterating on list- checking 
+  if id given equal to id in list then update the check status to opposite state
+  if id not equal then simply return as it is t 
+  */
   const toggleTask = (id) => {
     setTasks(prevState => prevState.map(t => (
       t.id === id
@@ -23,17 +30,54 @@ function App() {
     )))
   }
 
+  const updateTask = (task) => {
+    setTasks(prevState => prevState.map(t => (
+      t.id === task.id
+        ? { ...t, name: task.name }
+        : t
+    )))
+    closeEditMode();
+  }
+  const closeEditMode =() =>{
+    setisEditing(false);
+    // todo : previous state focus
+    isPreviousFocusEl.focus();
+  }
+ 
+  // const closeEditTask =(task) =>{
+  //   setEditedTask(task);
+  //   setisEditing(true);
+  // }
+  //close the edit mode
+  const enterEditMode=(task) =>{
+    setEditedTask(task);
+    setisEditing(true);
+    setPreviousFocusEl(document.activeElement);
+
+    //todo: set focus back to original and save updated task
+  }
+
   return (
     <div className="container">
       <header>
         <h1>My Task List</h1>
       </header>
+      {
+        isEditing && (
+          <EditForm 
+          editedTask={editedTask}
+          updateTask={updateTask}
+        />
+        )
+      }
+      
       <CustomForm addTask={addTask}/>
       {tasks && (
         <TaskList
           tasks={tasks}
           deleteTask={deleteTask}
           toggleTask={toggleTask}
+          enterEditMode={enterEditMode}
         />
       )}
     </div>
--- src/Components/TaskItem.jsx
+++ src/Components/TaskItem.jsx
@@ -2,62 +2,60 @@ import styles from './TaskItem.module.css';
 import { CheckIcon } from '@heroicons/react/24/outline';
 import { useState } from 'react';
 import { PencilIcon } from '@heroicons/react/24/outline';
-import { Pencil } from 'heroicons-react';
 import { TrashIcon } from '@heroicons/react/24/outline';
 
-const TaskItem= ({task,deleteTask}) =>
-{
+        const TaskItem= ({task,deleteTask, toggleTask, enterEditMode}) =>
+        {
 
-const [isChecked, setIsChecked] =useState(task.checked);
+        const [isChecked, setIsChecked] =useState(task.checked);
 
-const handleCheckboxChange= (e) =>{
-        setIsChecked(!isChecked);
+        const handleCheckboxChange= (e) =>{
+                setIsChecked(!isChecked);
+                toggleTask(task.id);
+        }
+        return ( 
+                <li className={styles.task}>
+                        <div className="task-group">
+                                <input type ="checkbox" className={styles.checkbox}
+                                checked={task.checked}
+                                name={task.name}
+                                id={task.id}
+                                onChange={handleCheckboxChange}
+                                />
+                                <label 
+                                htmlFor={task.id}
+                                className={styles.label}
+                                >
+                                {task.name}
+                                        <p className={styles.checkmark}>
+                                        <CheckIcon strokeWidth={2} width={24} height={24}/>
+
+                                        </p>
+                                </label>
+
+
+                        </div>
+                        <div className="task-group">
+                                <button className='btn'
+                                aria-label={`Update ${task.name} Task`}
+                                onClick={()=> enterEditMode(task)}
+                                >
+                                <PencilIcon width={24} height={24}/>
+                                </button>
+
+                                <button className={`btn ${styles.delete}`}
+                                aria-label={`Delete ${task.name} Task`}
+                                onClick={()=>deleteTask(task.id)}
+
+                                >
+                                <TrashIcon width={24} height={24}/>
+                                </button>
+
+                        </div>
+                </li>
+                        
+        )
 }
-return ( 
-<li className={styles.task}>
-<div className="task-group">
-<input type ="checkbox" className={styles.checkbox}
-checked={task.checked}
-name={task.name}
-id={task.id}
-onChange={handleCheckboxChange}
-/>
-<label 
-htmlFor={task.id}
-className={styles.label}
->
-{task.name}
-<p className={styles.checkmark}>
-<CheckIcon strokeWidth={2} width={24} height={24}/>
-
-</p>
-</label>
-
-
-</div>
-<div className="task-group">
-<button className='btn'
-aria-label={`Update ${task.name} Task`}
-//onClick={}
-
->
-<PencilIcon width={24} height={24}/>
-</button>
-
-<button className={`btn ${styles.delete}`}
-aria-label={`Delete ${task.name} Task`}
-onClick={()=>deleteTask(task.id)}
-
->
-<TrashIcon width={24} height={24}/>
-</button>
-
-</div>
-</li>
-        
-)
-}
-
 
 
 export default TaskItem
